f56778d2a9a7ac96b37d7672920d189f065f7aff
Brandon Davies <93037285+Da-lat@users.noreply.github.com>
2022-07-13 15:16:16 +0100

    Update __init__.py

--- cmt120-coursework-b-main/blog/__init__.py
+++ cmt120-coursework-b-main/blog/__init__.py
@@ -8,7 +8,7 @@ app.config['SECRET_KEY'] = 'c52ed441a1e4eacfcddd11a81a7ff38e67ac92a77a309d6d'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://USERNAME:PASSWORD@csmysql.cs.cf.ac.uk:3306/USERNAME_DATABASE_NAME'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c21118494:Liverpool2003!@csmysql.cs.cf.ac.uk:3306/c21118494_Flask_lab_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c21118494:PASSWORD@csmysql.cs.cf.ac.uk:3306/c21118494_Flask_lab_db'
 
 app.config['FLASKY_COMMENTS_PER_PAGE'] = 10
 db = SQLAlchemy(app)
